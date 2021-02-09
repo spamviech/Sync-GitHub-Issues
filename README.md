550bbff2a0b3c04f8c4e24559be95ca63c783467
spamviech <spamviech@web.de>
2021-02-09 16:43:28 +0100

    init stack/cabal project

---
+++ README.md
@@ -0,0 +1,1 @@
+# Sync-Github-Issues
---
+++ src/Main.hs
@@ -0,0 +1,9 @@
+module Main where
+
+main :: IO ()
+main = do
+  putStrLn "hello world"
+
+-- curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/spamviech/Zugkontrolle/issues
+-- https://docs.github.com/en/rest/reference/issues#list-repository-issues
+-- https://github.com/phadej/github/tree/master/samples/Issues
