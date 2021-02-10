8a58d2684f17127f357791a6d063c111efb99ee2
spamviech <spamviech@web.de>
2021-02-10 14:45:14 +0100

    prepare to parse remote owner/repository from .git/config

--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,21 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Main where
 
 import Control.Monad (forM_)
+import qualified Data.HashMap.Strict as HashMap
+import qualified Data.Text.IO as Text
 -- import qualified Data.Vector as Vector
 -- import qualified Data.ByteString as ByteString
 import GitHub (github  {-Issue(..), NewIssue(..),, IssueNumber(..)-})
 import qualified GitHub
 import Options.Applicative (help, info, long, metavar, progDesc, short, showDefault, strOption
-                          , value, execParser, helper, Parser)
-import System.Directory
+                          , value, execParser, helper, Parser())
+import System.Directory (createDirectoryIfMissing, doesFileExist, listDirectory)
+import Text.GitConfig.Parser (parseConfig, Section(..), GitConfig)
+import Text.Megaparsec ()
 import Token (token)
 
 data Repository =
@@ -21,17 +26,28 @@ data Repository =
     }
     deriving (Show, Eq)
 
-repositoryOption :: Parser Repository
-repositoryOption = Repository <$> ownerOption <*> repoOption <*> directoryOption
+repositoryOption
+    :: Maybe (GitHub.Name GitHub.Owner) -> Maybe (GitHub.Name GitHub.Repo) -> Parser Repository
+repositoryOption localOwner localName =
+    Repository <$> ownerOption localOwner <*> nameOption localName <*> directoryOption
 
-ownerOption :: Parser (GitHub.Name GitHub.Owner)
-ownerOption =
-    strOption $ long "owner" <> short 'o' <> metavar "OWNER" <> help "owner of the repository"
-
-repoOption :: Parser (GitHub.Name GitHub.Repo)
-repoOption =
+ownerOption :: Maybe (GitHub.Name GitHub.Owner) -> Parser (GitHub.Name GitHub.Owner)
+ownerOption remoteOwner =
     strOption
-    $ long "repository" <> short 'r' <> metavar "REPOSITORY" <> help "name of the repository"
+    $ long "owner"
+    <> short 'o'
+    <> metavar "OWNER"
+    <> help "owner of the repository"
+    <> maybe mempty value remoteOwner
+
+nameOption :: Maybe (GitHub.Name GitHub.Repo) -> Parser (GitHub.Name GitHub.Repo)
+nameOption remoteRepository =
+    strOption
+    $ long "repository"
+    <> short 'r'
+    <> metavar "REPOSITORY"
+    <> help "name of the repository"
+    <> maybe mempty value remoteRepository
 
 directoryOption :: Parser FilePath
 directoryOption =
@@ -43,10 +59,29 @@ directoryOption =
     <> value "Issues"
     <> showDefault
 
+extractFromConfig
+    :: Either e GitConfig -> (Maybe (GitHub.Name GitHub.Owner), Maybe (GitHub.Name GitHub.Repo))
+extractFromConfig (Left _error) = (Nothing, Nothing)
+extractFromConfig (Right config) = go (Nothing, Nothing) config
+    where
+        go :: (Maybe (GitHub.Name GitHub.Owner), Maybe (GitHub.Name GitHub.Repo))
+           -> GitConfig
+           -> (Maybe (GitHub.Name GitHub.Owner), Maybe (GitHub.Name GitHub.Repo))
+        go acc [] = acc
+        go acc (Section ("remote":_remoteName) entries:t) = case HashMap.lookup "url" entries of
+            (Just url) -> error $ show url -- TODO
+            Nothing -> go acc t
+        go acc (_h:t) = go acc t
+
+        -- Section ["remote","origin"] (fromList [("fetch","+refs/heads/*:refs/remotes/origin/*"),("url","git@github.com:spamviech/Zugkontrolle.git")])
 main :: IO ()
 main = do
+    let gitConfig = "../.git/config"
+    (localOwner, localRepository) <- doesFileExist gitConfig >>= \case
+        True -> extractFromConfig . parseConfig <$> Text.readFile gitConfig
+        False -> pure (Nothing, Nothing)
     Repository {owner, repository, directory} <- execParser
-        $ info (helper <*> repositoryOption)
+        $ info (helper <*> repositoryOption localOwner localRepository)
         $ progDesc "Sync issues of the specified github.com repository with a local copy."
     let aut = GitHub.OAuth token
     createDirectoryIfMissing False directory
