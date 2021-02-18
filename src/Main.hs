b1aa72f7188280f816d6d1f77ebb5190d11042d0
spamviech <spamviech@web.de>
2021-02-18 11:50:29 +0100

    get structure for actual syncing process

--- src/Main.hs
+++ src/Main.hs
@@ -36,48 +36,30 @@ main = do
             Left err -> do
                 hPrint stderr err
                 exitWith ConnectionError
-            Right issues -> pure issues
+            Right remoteIssues -> pure remoteIssues
         else pure HashMap.empty
-    (localOpenIssues, localClosedIssues) <- runExceptT (readIssues filePath) >>= \case
+    localIssues <- runExceptT (readIssues filePath) >>= \case
         Left err -> do
             hPutStrLn stderr $ "Parse Error: " ++ err
             exitWith ParseFileError
         Right issues -> pure issues
-    print localOpenIssues
-    print localClosedIssues
-    -- TODO create new issues
-    -- createIssueR :: Name Owner -> Name Repo -> NewIssue -> Request RW Issue
-    -- newIssue <- github aut
-    --     $ GitHub.createIssueR owner repository
-    --     $ NewIssue
-    --     { newIssueTitle = "Test"
-    --     , newIssueBody = Nothing
-    --     , newIssueAssignees = Vector.empty
-    --     , newIssueMilestone = Nothing
-    --     , newIssueLabels = Nothing
-    --     }
-    -- TODO edit changed issues
-    -- print newIssue
-    -- issueR :: Name Owner -> Name Repo -> IssueNumber -> Request k Issue
-    -- issue <- github aut $ GitHub.issueR owner repository $ IssueNumber 3
-    -- print issue
-    -- editIssueR :: Name Owner -> Name Repo -> IssueNumber -> EditIssue -> Request RW Issue
-    -- editOfIssue :: EditIssue
-    -- createCommentR :: Name Owner -> Name Repo -> IssueNumber -> Text -> Request RW Comment
-    -- editCommentR :: Name Owner -> Name Repo -> Id Comment -> Text -> Request RW Comment
-    -- curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/spamviech/Zugkontrolle/issues
-    -- https://docs.github.com/en/rest/reference/issues#list-repository-issues
-    -- https://github.com/phadej/github/tree/master/samples/Issues
-    -- TODO write file Issues.txt
-    -- let syncedIssues = (remoteIssues, HashMap.empty)
-    let syncedIssues =
-            (HashMap.map fst $ fst localOpenIssues, HashMap.map fst $ fst localClosedIssues)
-    -- let writeFailure = "Failed to write to \"" ++ filePath ++ "\""
-    when writeToFile $ writeIssues filePath syncedIssues
+    syncedIssues <- if updateGitHub
+        then runExceptT (applyRemoteChanges aut $ calculateChanges remoteIssues localIssues)
+            >>= \case
+                Left err -> do
+                    hPrint stderr err
+                    exitWith ConnectionError
+                Right syncedIssues -> pure syncedIssues
+        else pure $ (\(_a, _b, _c_, _d, e) -> e) $ calculateChanges remoteIssues localIssues
+    if writeToFile
+        then writeIssues filePath syncedIssues
+        else print syncedIssues
     exitWith Success
     where
         -- TODO Dev-Flags so file-overwrites/api-access can be restricted
         queryGitHub = False
+
+        updateGitHub = False
 
         writeToFile = False
 
@@ -107,12 +89,54 @@ queryIssues aut owner repository = do
         lfNewlines :: Text -> Text
         lfNewlines = Text.replace "\r\n" "\n"
 
-calculateChanges :: ( [GitHub.EditIssue]    -- editIssueR
-                    , [GitHub.NewIssue]     -- createIssueR
-                    , [(GitHub.Id GitHub.Comment, Text)]    -- editCommentR
-                    , [(GitHub.IssueNumber, Text)]          -- createCommentR
-                    , ( HashMap GitHub.IssueNumber LocalIssue
-                      , HashMap GitHub.IssueNumber LocalIssue
-                      ) -- syncedIssues
-                    )
-calculateChanges = undefined --TODO
+calculateChanges
+    :: HashMap GitHub.IssueNumber LocalIssue
+    -> ( (HashMap GitHub.IssueNumber (LocalIssue, [LocalComment]), [(LocalIssue, [LocalComment])])
+       , (HashMap GitHub.IssueNumber (LocalIssue, [LocalComment]), [(LocalIssue, [LocalComment])])
+       )
+    -> ( [GitHub.EditIssue]                             -- editIssueR
+       , [(GitHub.NewIssue, [Text], GitHub.IssueState)] -- createIssueR, createCommentR
+       , [(GitHub.Id GitHub.Comment, Text)]             -- editCommentR
+       , [(GitHub.IssueNumber, Text)]                   -- createCommentR
+       , (HashMap GitHub.IssueNumber LocalIssue, HashMap GitHub.IssueNumber LocalIssue) -- unchangedIssues
+       )
+calculateChanges remoteOpenIssues (localOpenIssues, localClosedIssues) = undefined --TODO
+
+applyRemoteChanges
+    :: (GitHub.AuthMethod auth)
+    => auth
+    -> ( [GitHub.EditIssue]                             -- editIssueR
+       , [(GitHub.NewIssue, [Text], GitHub.IssueState)] -- createIssueR, createCommentR
+       , [(GitHub.Id GitHub.Comment, Text)]             -- editCommentR
+       , [(GitHub.IssueNumber, Text)]                   -- createCommentR
+       , (HashMap GitHub.IssueNumber LocalIssue, HashMap GitHub.IssueNumber LocalIssue) -- unchangedIssues
+       )
+    -> ExceptT
+        GitHub.Error
+        IO
+        (HashMap GitHub.IssueNumber LocalIssue, HashMap GitHub.IssueNumber LocalIssue)
+applyRemoteChanges aut changes = do
+    -- TODO create new issues
+    -- createIssueR :: Name Owner -> Name Repo -> NewIssue -> Request RW Issue
+    -- newIssue <- github aut
+    --     $ GitHub.createIssueR owner repository
+    --     $ NewIssue
+    --     { newIssueTitle = "Test"
+    --     , newIssueBody = Nothing
+    --     , newIssueAssignees = Vector.empty
+    --     , newIssueMilestone = Nothing
+    --     , newIssueLabels = Nothing
+    --     }
+    -- TODO edit changed issues
+    -- print newIssue
+    -- issueR :: Name Owner -> Name Repo -> IssueNumber -> Request k Issue
+    -- issue <- github aut $ GitHub.issueR owner repository $ IssueNumber 3
+    -- print issue
+    -- editIssueR :: Name Owner -> Name Repo -> IssueNumber -> EditIssue -> Request RW Issue
+    -- editOfIssue :: EditIssue
+    -- createCommentR :: Name Owner -> Name Repo -> IssueNumber -> Text -> Request RW Comment
+    -- editCommentR :: Name Owner -> Name Repo -> Id Comment -> Text -> Request RW Comment
+    -- curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/spamviech/Zugkontrolle/issues
+    -- https://docs.github.com/en/rest/reference/issues#list-repository-issues
+    -- https://github.com/phadej/github/tree/master/samples/Issues
+    undefined --TODO
